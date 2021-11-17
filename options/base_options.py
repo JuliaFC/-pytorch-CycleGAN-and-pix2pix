7f9ca24c194794252084e06f000b7b3bf5c4b561
JuliaFC <mjfc@cin.ufpe.br>
2021-11-17 16:41:40 -0300

    Change print_options method

--- options/base_options.py
+++ options/base_options.py
@@ -92,24 +92,7 @@ class BaseOptions():
         It will print both current options and default values(if different).
         It will save options into a text file / [checkpoints_dir] / opt.txt
         """
-        message = ''
-        message += '----------------- Options ---------------\n'
-        for k, v in sorted(vars(opt).items()):
-            comment = ''
-            default = self.parser.get_default(k)
-            if v != default:
-                comment = '\t[default: %s]' % str(default)
-            message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
-        message += '----------------- End -------------------'
-        print(message)
-
-        # save to the disk
-        expr_dir = os.path.join(opt.checkpoints_dir, opt.name)
-        util.mkdirs(expr_dir)
-        file_name = os.path.join(expr_dir, '{}_opt.txt'.format(opt.phase))
-        with open(file_name, 'wt') as opt_file:
-            opt_file.write(message)
-            opt_file.write('\n')
+        print("As imagens finais serão carregadas em breve...")
 
     def parse(self):
         """Parse our options, create checkpoints directory suffix, and set up gpu device."""
